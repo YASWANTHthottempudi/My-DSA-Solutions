bb9c79f6e2324fe749ea72c3aaa999d2c6f7fe38
YASWANTH THOTTEMPUDI <95462190+YASWANTHthottempudi@users.noreply.github.com>
2025-11-13 17:57:05 -0600

    Add preorder traversal method in Solution class
    Implement preorder traversal for binary tree.

---
+++ 144. Binary Tree Preorder Traversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        l=[]
+        node=root
+        def traversal(node):
+            if not node:
+                return 
+            l.append(node.val)
+            traversal(node.left)
+            traversal(node.right)
+
+        traversal(root)
+        return l
+
+        
