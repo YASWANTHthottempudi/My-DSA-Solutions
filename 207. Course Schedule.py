5467f255cf7481a367a5de1f97d2d07951a6949e
YASWANTH THOTTEMPUDI <95462190+YASWANTHthottempudi@users.noreply.github.com>
2026-02-16 10:16:39 -0600

    Implement Course Schedule solution using BFS

---
+++ 207. Course Schedule.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
+        adj=[[] for _ in range(n)]
+        indeg=[0]*n
+        res=[]
+        
+        for val in prerequisites:
+            course=val[0]
+            prereq= val[1]
+            adj[prereq].append(course)
+            indeg[course]+=1
+        
+        queue=deque()
+        for i in range(n):
+            if indeg[i]==0:
+                queue.append(i)
+
+        while queue:
+            cur=queue.popleft()
+            res.append(cur)
+
+            for nextcourse in adj[cur]:
+                print(nextcourse)
+                indeg[nextcourse]-=1
+                if indeg[nextcourse]==0:
+                    queue.append(nextcourse)
+
+        return len(res)==n
+        
